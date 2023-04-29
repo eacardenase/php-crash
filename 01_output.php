46a472e37af82efae19e93ee9865ca0d5642aa6e
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-29 18:32:08 -0500

    Initial setup

---
+++ 01_output.php
@@ -0,0 +1,27 @@
+<?php
+/* -------- Output & Comments ------- */
+
+// Single line comment
+/*
+ * Multiline
+ * comment
+ */
+
+// echo - Output strings, numbers, html, etc
+//echo 123, 'Hello', 10.5;
+
+// print - Works like echo, but can only take in a single argument
+//print 123;
+
+// print_r() - Print single values and arrays
+//print_r([1, 2, 3]);
+
+// var_dump() - Returns more info like data type and length
+//var_dump('Hello');
+
+// var_export() - Similar to var_dump(). Outputs a string representation of a variable
+var_export('Hello');
+
+/*
+  A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
+*/
